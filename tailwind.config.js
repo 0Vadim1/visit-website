dc1c24ee166a73cd93ca6fc50b341f584ccc57cc
0Vadim1 <107241427+0Vadim1@users.noreply.github.com>
2026-03-22 14:48:26 +0200

    Version 5.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
